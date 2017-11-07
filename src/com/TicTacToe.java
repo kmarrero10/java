9b4294d0d1ee28e8e75a6d6221b5d34c4312f1c0
kmarrero10 <kmarrero1994@gmail.com>
2017-11-07 14:23:34 -0600

    Add files via upload

---
+++ src/com/TicTacToe.java
@@ -0,0 +1,34 @@
+package com.kylemarrero;
+
+/**
+ * Created by kylemarrero on 7/18/17.
+ */
+public class TicTacToe {
+    public static void main(String[] args) {
+        GameBoard myGame = new GameBoard();
+       int counter = 1;
+
+        while (myGame.gameIsGoing() && counter < 10) {
+            if (counter % 2 == 0) {
+                myGame.machineMove(counter);
+            }
+            else myGame.askPlayer('X');
+            counter++;
+
+            System.out.println("\n");
+            myGame.displayBoard();
+            myGame.checkWinner();
+
+
+            if (counter == 10) {
+                System.out.println("It's a draw!");
+            }
+        }
+
+        if (counter % 2 == 0) {
+            myGame.writeFile();
+        }
+
+
+    }
+}
